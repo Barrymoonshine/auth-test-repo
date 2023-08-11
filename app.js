31e6f4792da7e31d1caeca64da1e3e0f27bfa875
Nick Sheward <nickshwrd@gmail.com>
2023-08-11 14:51:26 +0100

    Added log in/log out functionality

--- app.js
+++ app.js
@@ -24,12 +24,44 @@ const User = mongoose.model(
 const app = express();
 app.set('view engine', 'ejs');
 
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: 'Incorrect password' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => {
+  res.render('index', { user: req.user });
+});
 
 app.get('/sign-up-form', (req, res) => res.render('sign-up-form'));
 
@@ -44,6 +76,23 @@ app.post('/sign-up-form', async (req, res, next) => {
   } catch (err) {
     return next(err);
   }
+});
+
+app.post(
+  '/log-in',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/',
+  })
+);
+
+app.get('/log-out', (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
 });
 
 app.listen(3000, () => console.log('app listening on port 3000!'));
