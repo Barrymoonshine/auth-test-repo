1f04a924d32089f042a52e011e15436152e5311c
Nick Sheward <nickshwrd@gmail.com>
2023-08-11 11:42:51 +0100

    Boiler plate from lesson saved in app.js

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
+import 'dotenv/config';
+import path from 'path';
 import express from 'express';
+import mongoose from 'mongoose';
+import session from 'express-session';
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
 
-// Set up Express app
+const { Schema } = mongoose;
+
+const mongoDb = `mongodb+srv://${process.env.DB_CREDENTIALS}@cluster0.wym9xjg.mongodb.net/auth-test?retryWrites=true&w=majority`;
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'));
+
+const User = mongoose.model(
+  'User',
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+  })
+);
+
 const app = express();
-
-// Listen for requests
-app.listen(3000);
-
-// Middleware & static files
-app.use(express.static('public'));
-
-// Register View Engine (EJS)
+app.set('views', __dirname);
 app.set('view engine', 'ejs');
 
-// Render a view
-app.get('/', (req, res) => {
-  res.render('index');
-});
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.render('index'));
+
+app.listen(3000, () => console.log('app listening on port 3000!'));
